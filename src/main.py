56a32fdde8081fd5e0e415608fe69f6c3a99bd57
parishijainn <jain.parishi@gmail.com>
2025-04-12 18:36:19 -0400

    Update main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from cmu_graphics import *
+
+def onAppStart(app):
+    app.color = 'purple'
+    app.x = 0
+    app.y = 0
+    app.speed = 5
+    app.direction = 0
+
+
+def redrawAll(app):
+    drawRect(app.x, app.y, 20, 20, fill=app.color)
+
+runApp()
