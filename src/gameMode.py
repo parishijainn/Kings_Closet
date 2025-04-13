ef7d2a64b3c93d217eaa7f34a89686c63e3feef5
parishijainn <jain.parishi@gmail.com>
2025-04-13 10:31:45 -0400

    Fixing formatting of drawGameMode

--- src/gameMode.py
+++ src/gameMode.py
@@ -30,68 +30,141 @@ app.playButtonWidth = app.forwardButtonWidth*0.66
 app.playButtonHeight = app.forwardButtonHeight
 app.playButtonX = app.width/2 - app.playButtonWidth/2
 app.playButtonY = app.forwardButtonY
+app.feedbackText = ""
+
+# def drawGameMode(app):
+#     #app.modeButtonWidth, app.modeButtonHeight = 160, 80
+#     #app.blackBarHeight = 50
+#     drawImage("images/cheetahBackground.png", 0, 0,
+#                  width=app.width, height=app.height)
+#     drawRect(0, 0, app.width, app.blackBarHeight, fill='black')
+#     drawRect(0, app.height-app.blackBarHeight, app.width,
+#              app.blackBarHeight, fill='black')
+#     drawRect(app.width/3, app.blackBarHeight, app.width/3, 
+#              app.height-app.blackBarHeight*2, fill='white')
+#     #browse mode and dress me mode buttons
+#     drawRect(2*(app.width/3), 
+#              app.height-app.blackBarHeight-app.modeButtonHeight, 
+#              app.modeButtonWidth, app.modeButtonHeight, fill='gray')
+#     drawRect((app.width/3)-app.modeButtonWidth, 
+#              app.height-app.blackBarHeight-app.modeButtonHeight, 
+#              app.modeButtonWidth, app.modeButtonHeight, fill='gray')
+    
+    
+#     if app.isSelectionMode:
+#         drawRect(app.width/3, (app.height/2)-app.blackBarHeight, 
+#                  app.width/3, app.blackBarHeight, fill='black')
+#         drawRect(app.width/3, app.height-app.blackBarHeight*2, app.width/3, 
+#                  app.blackBarHeight, fill='black')
+        
+#         #shirts and pants play buttons
+#         drawRect(app.playButtonX, app.playButtonY, app.playButtonWidth,
+#                  app.playButtonHeight, fill='gray')
+#         drawRect(app.playButtonX, app.playButtonY+app.whiteBoxHeight/2,
+#                  app.playButtonWidth, app.playButtonHeight, fill='gray')
+#         #pants and shirts backwards buttons
+#         drawRect(app.forwardButtonX, app.forwardButtonY, app.forwardButtonWidth,
+#                  app.forwardButtonHeight, fill='gray')
+#         drawRect(app.forwardButtonX, app.forwardButtonY+app.whiteBoxHeight/2, 
+#                  app.forwardButtonWidth, app.forwardButtonHeight, fill='gray')
+#         #pants and shirts forward buttons
+#         drawRect(app.backwardButtonX, 
+#                  app.forwardButtonY , app.forwardButtonWidth, 
+#                  app.forwardButtonHeight, fill='gray')
+#         drawRect(app.backwardButtonX,
+#                  app.forwardButtonY+app.whiteBoxHeight/2, 
+#                  app.forwardButtonWidth, app.forwardButtonHeight, fill='gray')
+#         #images of tops and bottoms
+#         drawImage(app.tops[app.currTopIndex].image, app.width/2, 
+#                   ((app.height/2)-app.blackBarHeight*2), width=app.width/3, 
+#                   height=((app.height/2)-app.blackBarHeight*2)/2, align='center')
+#         drawImage(app.bottoms[app.currBottomIndex].image, app.width/2, 
+#                   (app.height-app.blackBarHeight)/2, width=app.width/3, 
+#                   height=((app.height/2)-app.blackBarHeight*2), align='center')
+
+#     if app.isDressingMode:
+#          drawImage("images/mannequin.png", 
+#                  app.width/2, app.height/2-10,
+#                  width=app.width/3, height=app.height-3*app.blackBarHeight,
+#                  align='center')
+#          drawImage(app.tops[app.currTopIndex].image, app.width/2, 
+#                   ((app.height/2)-app.blackBarHeight*2), width=app.width/3, 
+#                   height=((app.height/2)-app.blackBarHeight*2)/2, align='center')
+#          drawImage(app.bottoms[app.currBottomIndex].image, app.width/2, 
+#                   (app.height-app.blackBarHeight)/2, width=app.width/3, 
+#                   height=((app.height/2)-app.blackBarHeight*2), align='center')
+
 
 def drawGameMode(app):
-    #app.modeButtonWidth, app.modeButtonHeight = 160, 80
-    #app.blackBarHeight = 50
     drawImage("images/cheetahBackground.png", 0, 0,
-                 width=app.width, height=app.height)
+              width=app.width, height=app.height)
     drawRect(0, 0, app.width, app.blackBarHeight, fill='black')
     drawRect(0, app.height-app.blackBarHeight, app.width,
              app.blackBarHeight, fill='black')
-    drawRect(app.width/3, app.blackBarHeight, app.width/3, 
+    drawRect(app.width/3, app.blackBarHeight, app.width/3,
              app.height-app.blackBarHeight*2, fill='white')
-    #browse mode and dress me mode buttons
+
+    # Mode switch buttons
     drawRect(2*(app.width/3), 
              app.height-app.blackBarHeight-app.modeButtonHeight, 
              app.modeButtonWidth, app.modeButtonHeight, fill='gray')
     drawRect((app.width/3)-app.modeButtonWidth, 
              app.height-app.blackBarHeight-app.modeButtonHeight, 
              app.modeButtonWidth, app.modeButtonHeight, fill='gray')
-    
-    
+
     if app.isSelectionMode:
         drawRect(app.width/3, (app.height/2)-app.blackBarHeight, 
                  app.width/3, app.blackBarHeight, fill='black')
         drawRect(app.width/3, app.height-app.blackBarHeight*2, app.width/3, 
                  app.blackBarHeight, fill='black')
-        
-        #shirts and pants play buttons
+
+        # Navigation and selection buttons
         drawRect(app.playButtonX, app.playButtonY, app.playButtonWidth,
                  app.playButtonHeight, fill='gray')
         drawRect(app.playButtonX, app.playButtonY+app.whiteBoxHeight/2,
                  app.playButtonWidth, app.playButtonHeight, fill='gray')
-        #pants and shirts backwards buttons
+
         drawRect(app.forwardButtonX, app.forwardButtonY, app.forwardButtonWidth,
                  app.forwardButtonHeight, fill='gray')
         drawRect(app.forwardButtonX, app.forwardButtonY+app.whiteBoxHeight/2, 
                  app.forwardButtonWidth, app.forwardButtonHeight, fill='gray')
-        #pants and shirts forward buttons
-        drawRect(app.backwardButtonX, 
-                 app.forwardButtonY , app.forwardButtonWidth, 
-                 app.forwardButtonHeight, fill='gray')
-        drawRect(app.backwardButtonX,
-                 app.forwardButtonY+app.whiteBoxHeight/2, 
+
+        drawRect(app.backwardButtonX, app.forwardButtonY , 
                  app.forwardButtonWidth, app.forwardButtonHeight, fill='gray')
-        #images of tops and bottoms
-        drawImage(app.tops[app.currTopIndex].image, app.width/2, 
-                  ((app.height/2)-app.blackBarHeight*2), width=app.width/3, 
-                  height=((app.height/2)-app.blackBarHeight*2)/2, align='center')
-        drawImage(app.bottoms[app.currBottomIndex].image, app.width/2, 
-                  (app.height-app.blackBarHeight)/2, width=app.width/3, 
-                  height=((app.height/2)-app.blackBarHeight*2), align='center')
+        drawRect(app.backwardButtonX, app.forwardButtonY+app.whiteBoxHeight/2, 
+                 app.forwardButtonWidth, app.forwardButtonHeight, fill='gray')
+
+        # Draw outfit images (fixed positioning)
+        topY = 140
+        bottomY = 395
+        imgWidth = 180
+        imgHeight = 180
+        drawImage(app.tops[app.currTopIndex].image, app.width//2, topY,
+                  width=imgWidth, height=imgHeight, align='center')
+        drawImage(app.bottoms[app.currBottomIndex].image, app.width//2, bottomY,
+                  width=imgWidth, height=imgHeight, align='center')
+
+        # Feedback text
+        drawLabel(app.feedbackText, app.width//2, app.height - 100,
+                  size=22, fill='darkmagenta')
+
+        # Grade button
+        drawRect(app.width/2 - 50, app.height - app.blackBarHeight - 60,
+                 100, 40, fill='plum', border='black')
+        drawLabel("Grade", app.width/2, app.height - app.blackBarHeight - 40,
+                  size=18, bold=True)
 
     if app.isDressingMode:
-         drawImage("images/mannequin.png", 
-                 app.width/2, app.height/2-10,
-                 width=app.width/3, height=app.height-3*app.blackBarHeight,
-                 align='center')
-         drawImage(app.tops[app.currTopIndex].image, app.width/2, 
-                  ((app.height/2)-app.blackBarHeight*2), width=app.width/3, 
-                  height=((app.height/2)-app.blackBarHeight*2)/2, align='center')
-         drawImage(app.bottoms[app.currBottomIndex].image, app.width/2, 
-                  (app.height-app.blackBarHeight)/2, width=app.width/3, 
-                  height=((app.height/2)-app.blackBarHeight*2), align='center')
+        drawImage("images/mannequin.png", 
+                  app.width/2, app.height/2-10,
+                  width=app.width/3, height=app.height-3*app.blackBarHeight,
+                  align='center')
+        drawImage(app.tops[app.currTopIndex].image, app.width/2, 140,
+                  width=180, height=180, align='center')
+        drawImage(app.bottoms[app.currBottomIndex].image, app.width/2, 320,
+                  width=180, height=180, align='center')
+
+
     
 #def onMousePress(app, mouseX, mouseY):
     #if app.state == 'gameMode':    
