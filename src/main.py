553a9a441484bc1ae7921f1c7418dbd4a0c6af95
lilyana-s <lilyanasponhouse@gmail.com>
2025-04-12 22:38:13 -0400

    main fix

--- src/main.py
+++ src/main.py
@@ -40,38 +40,3 @@ def redrawAll(app):
         drawMainGame(app)
 
 runApp(width=800, height=600)
-
-# from cmu_graphics import *
-# from objects import VideoManager, OutfitManager
-# from ui import drawWelcomeScreen, drawMainGame
-# import os
-
-# def onAppStart(app):
-#     # Setup game dimensions
-#     app.width = 800
-#     app.height = 600
-#     app.state = "welcome"
-    
-#     # Initialize managers
-#     app.videoManager = VideoManager(app)
-#     app.outfitManager = OutfitManager(app)
-
-# def onMousePress(app, mouseX, mouseY):
-#     # Check if start button was clicked
-#     if (app.state == "welcome" and 
-#         abs(mouseX - app.startButton['x']) < app.startButton['width']/2 and
-#         abs(mouseY - app.startButton['y']) < app.startButton['height']/2):
-#         app.state = "main"
-#         app.videoManager.stopVideo()  # Stop video when leaving welcome screen
-
-# def onStep(app):
-#     if app.state == "welcome":
-#         app.videoManager.update()
-
-# def redrawAll(app):
-#     if app.state == "welcome":
-#         drawWelcomeScreen(app)
-#     elif app.state == "main":
-#         drawMainGame(app)
-
-# runApp(width=800, height=600)
